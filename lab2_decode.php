de089c7f96c23678842f25384a0073a502aeb3c4
Senados, R jay <senadosr-jay_bsit@plmun.edu.ph>
2025-10-20 14:47:08 +0800

    Delete lab2_decode.php

--- lab2_decode.php
+++
@@ -1,7 +0,0 @@
-<?php
-$json_string = '{"name":"Rodrigo","age":21,"email":"Rodrigo@example.com"}';
-$student_object = json_decode($json_string);
-$student_array = json_decode($json_string, true);
-echo "Object: " . $student_object->name . "<br>"; 
-echo "Array: " . $student_array["email"];          
-?>
